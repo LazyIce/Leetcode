774dbe3c8369b5f2f979357505d359056300525f
Pandaice <1542752218@qq.com>
2018-11-09 01:26:07 -0500

    two problems

--- src/DecodeWays.java
+++ src/DecodeWays.java
@@ -1,25 +1,45 @@
-/**
- * time:
- * space:
- */
 public class DecodeWays {
-    public int numDecodings(String s) {
-        int[] dp = new int[s.length()];
+    /**
+     * time: O(n)
+     * space: O(n)
+     */
+    public int numDecodings1(String s) {
+        int len = s.length();
+        int[] dp = new int[len + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+        for (int i = 2; i <= len; i++) {
+            int first = Integer.valueOf(s.substring(i-1, i));
+            int second = Integer.valueOf(s.substring(i-2, i));
+            if (first >= 1 && first <= 9) {
+                dp[i] += dp[i - 1];
+            }
+            if (second >= 10 && second <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+        return dp[len];
+    }
+
+    /**
+     * time: O(n)
+     * space: O(1)
+     */
+    public int numDecodings2(String s) {
         if (s.charAt(0) == '0')
             return 0;
-        dp[0] = 1;
+        int c1 = 1;
+        int c2 = 1;
         for (int i = 1; i < s.length(); i++) {
-            if (s.charAt(i) == '0' && s.charAt(i-1) > '2' || s.charAt(i) == '0' && s.charAt(i-1) == '0')
-                return 0;
-            else if (i > 1 && s.charAt(i) == '0')
-                dp[i] = dp[i-2];
-            else if (s.charAt(i) == '0')
-                dp[i] = 1;
-            else if (Integer.parseInt(s.substring(i-1, i+1)) > 10 && Integer.parseInt(s.substring(i-1, i+1)) <= 26)
-                dp[i] = dp[i-1] + 1;
-            else
-                dp[i] = dp[i-1];
+            if (s.charAt(i) == '0')
+                c1 = 0;
+            if (s.charAt(i - 1) == '1' || s.charAt(i - 1) == '2' && s.charAt(i) <= '6') {
+                c1 = c1 + c2;
+                c2 = c1 - c2;
+            } else {
+                c2 = c1;
+            }
         }
-        return dp[s.length()-1];
+        return c1;
     }
 }
