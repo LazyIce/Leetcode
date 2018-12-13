badced21cbcaf4cfc2e0e016fe7dcd021e919236
Pandaice <1542752218@qq.com>
2018-12-12 23:45:05 -0500

    two problems

--- src/ConstructBinaryTreeFromInorderAndPostiorderTree.java
+++ src/ConstructBinaryTreeFromInorderAndPostiorderTree.java
@@ -1,2 +1,27 @@
+/**
+ * time:
+ * space:
+ */
 public class ConstructBinaryTreeFromInorderAndPostiorderTree {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        if (inorder == null || postorder == null || inorder.length != postorder.length)
+            return null;
+        return helper(postorder.length - 1, 0, postorder.length - 1, postorder, inorder);
+    }
+
+    public TreeNode helper(int postEnd, int inStart, int inEnd, int[] postorder, int[] inorder) {
+        if (postEnd < 0 || inStart > inEnd) {
+            return null;
+        }
+        TreeNode cur = new TreeNode(postorder[postEnd]);
+        int i = inStart;
+        while (i <= inEnd) {
+            if (inorder[i] == postorder[postEnd])
+                break;
+            i++;
+        }
+        cur.left = helper(postEnd - inorder.length + i, inStart, i - 1, postorder, inorder);
+        cur.right = helper(postEnd - 1, i + 1, inEnd, postorder, inorder);
+        return cur;
+    }
 }
