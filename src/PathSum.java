afda2d88bda7ae97e54a40e9b60ff3b179ff021b
Pandaice <1542752218@qq.com>
2018-12-15 23:47:55 -0500

    two problems

---
+++ src/PathSum.java
@@ -0,0 +1,15 @@
+/**
+ * time:
+ * space:
+ */
+public class PathSum {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null)
+            return false;
+        return helper(root, sum);
+    }
+
+    public boolean helper(TreeNode node, int sum) {
+        if (root.left == null && )
+    }
+}
