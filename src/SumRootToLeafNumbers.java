58a8ccdafafc2d45995bcf63255fe407c4eff8dd
Pandaice <1542752218@qq.com>
2019-01-03 18:03:00 -0500

    four problems

---
+++ src/SumRootToLeafNumbers.java
@@ -0,0 +1,17 @@
+/**
+ * time: O(n)
+ * space: O(1)
+ */
+public class SumRootToLeafNumbers {
+    public int sumNumbers(TreeNode root) {
+        return sum(root, 0);
+    }
+
+    public int sum(TreeNode n, int s){
+        if (n == null)
+            return 0;
+        if (n.right == null && n.left == null)
+            return s*10 + n.val;
+        return sum(n.left, s*10 + n.val) + sum(n.right, s*10 + n.val);
+    }
+}
