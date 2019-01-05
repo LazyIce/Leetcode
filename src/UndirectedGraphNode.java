038e394ca8e32ec6bc02839bd817801fe60a9674
Pandaice <1542752218@qq.com>
2019-01-04 23:54:43 -0500

    two problems

---
+++ src/UndirectedGraphNode.java
@@ -0,0 +1,8 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class UndirectedGraphNode {
+    int label;
+    List<UndirectedGraphNode> neighbors;
+    UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+}
