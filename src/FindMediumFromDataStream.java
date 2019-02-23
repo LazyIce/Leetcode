78810727821ce4236e99a7925a9097995dcfa501
bxie41 <bin.xie@gatech.edu>
2019-02-22 23:45:09 -0500

    three problems

--- src/FindMediumFromDataStream.java
+++ src/FindMediumFromDataStream.java
@@ -1,3 +1,29 @@
-public class FindMediumFromDataStream {
+import java.util.Collections;
+import java.util.PriorityQueue;
 
+public class FindMediumFromDataStream {
+    private PriorityQueue<Integer> low = null;
+    private PriorityQueue<Integer> high = null;
+    /** initialize your data structure here. */
+    public FindMediumFromDataStream() {
+        low = new PriorityQueue<Integer>(Collections.reverseOrder());
+        high = new PriorityQueue<Integer>();
+    }
+
+    public void addNum(int num) {
+        low.offer(num);
+        high.offer(low.poll());
+
+        if (low.size() < high.size()){
+            low.offer(high.poll());
+        }
+    }
+
+    public double findMedian() {
+        if (low.size() > high.size()){
+            return (double) low.peek();
+        } else {
+            return (low.peek() + high.peek()) * 0.5;
+        }
+    }
 }
---
+++ src/JumpGame2.java
@@ -0,0 +1,15 @@
+public class JumpGame2 {
+    public int jumpCount(int[] nums) {
+        int res = 0;
+        int curMaxArea = 0;
+        int maxNext = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            maxNext = Math.max(maxNext, i + nums[i]);
+            if (i == curMaxArea) {
+                res++;
+                curMaxArea = maxNext;
+            }
+        }
+        return res;
+    }
+}
