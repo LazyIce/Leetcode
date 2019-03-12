fd93d0ee658f0457a6f8b15e2f750e4c36a8dcbf
LazyIce <1542752218@qq.com>
2019-03-11 23:55:11 -0400

    three problems

---
+++ src/LongestCommonPrefix.java
@@ -0,0 +1,24 @@
+/**
+ * time: O(n * m)
+ * space: O(1)
+ */
+public class LongestCommonPrefix {
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0)
+            return "";
+        String pre = strs[0];
+        int i = 1;
+        while(i < strs.length){
+            while(strs[i].indexOf(pre) != 0)
+                pre = pre.substring(0,pre.length()-1);
+            i++;
+        }
+        return pre;
+    }
+
+    public static void main(String[] args) {
+        String[] arr = new String[]{"flower", "flour", "flag"};
+        String res = longestCommonPrefix(arr);
+        System.out.println(res);
+    }
+}
