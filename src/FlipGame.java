21250b1c0d454a91368d46b027acf836a9a4d8f9
LazyIce <1542752218@qq.com>
2019-05-16 20:54:39 -0700

    three problems

---
+++ src/FlipGame.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FlipGame {
+    public List<String> generatePossibleNextMoves(String s) {
+        List<String> list = new ArrayList<String>();
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == '+' && s.charAt(i - 1) == '+') {
+                list.add(s.substring(0, i - 1) + "--" + s.substring(i + 1, s.length()));
+            }
+        }
+        return list;
+    }
+}
