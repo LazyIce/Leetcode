046c4ba9a520a7964aa662886e704e203c60913e
LazyIce <1542752218@qq.com>
2019-05-21 21:59:00 -0700

    add readme

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,20 +1,27 @@
-/**
- * time: O(n)
- * space: O(n)
- */
-
 import java.util.Map;
 import java.util.HashMap;
 
 public class TwoSum {
+    /**
+     * time: O(n)
+     * space: O(n)
+     */
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> m = new HashMap<Integer, Integer>();
-        for(int i =0; i < nums.length; ++i) {
-            int num = target - nums[i];
-            if(m.containsKey(num))
-                return new int[]{m.get(num), i};
-            m.put(nums[i], i);
+        int[] res = new int[]{-1, -1};
+        
+        if (nums == null || nums.length < 2) {
+            return res;
         }
-        throw new RuntimeException("no answer");
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+
+        return res;
     }
 }
