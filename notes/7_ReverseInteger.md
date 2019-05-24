1c5ea4a117d888e33d7129a04d6c3e7f2e7dff30
LazyIce <1542752218@qq.com>
2019-05-23 23:38:15 -0700

    reverse integer

---
+++ notes/7_ReverseInteger.md
@@ -0,0 +1,31 @@
+# Reverse Integer
+
+## Description
+
+Given a 32-bit signed integer, reverse digits of an integer.
+
+## Solution
+
+Here, think about corner case: out of bound. So we need use a long type to keep result.
+
+```java
+/**
+ * time complexity: O(logN)
+ * sapce complexity: O(1)
+ */
+public int reverse(int x) {
+    long res = 0;
+    while (x != 0) {
+        res = res * 10 + x % 10;
+        x = x / 10;
+        if (res > Integer.MAX_VALUE || res < Integer.MIN_VALUE)
+            return 0;
+    }
+    return (int)res;
+}
+```
+
+
+
+
+
--- src/ReverseInteger.java
+++ src/ReverseInteger.java
@@ -1,17 +1,12 @@
 public class ReverseInteger {
     public int reverse(int x) {
         long res = 0;
-        boolean positive = true;
-        if (x < 0)
-            positive = false;
-        int newX = Math.abs(x);
-        while (newX > 0) {
-            res = res * 10 + (newX % 10);
-            newX = newX / 10;
+        while (x != 0) {
+            res = res * 10 + x % 10;
+            x = x / 10;
+            if (res > Integer.MAX_VALUE || res < Integer.MIN_VALUE)
+                return 0;
         }
-        res = positive ? res : res * (-1);
-        if (res > Integer.MAX_VALUE || res < Integer.MIN_VALUE)
-            res = 0;
         return (int)res;
     }
 }
