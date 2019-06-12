8165f045613643488cf5b8f0f5f2496c72df71e7
LazyIce <1542752218@qq.com>
2019-06-11 23:45:50 -0700

    modify one problem

--- leetcode/_126_WordLadderII.java
+++ leetcode/_126_WordLadderII.java
@@ -2,36 +2,6 @@ import java.util.*;
 
 public class _126_WordLadderII {
     /**
-     * DFS
-     * time: O(n)
-     * space: O(n)
-     */
-    public List<String> wordBreak(String s, List<String> wordDict) {
-        return DFS(s, wordDict, new HashMap<String, LinkedList<String>>());
-    }
-
-    // DFS function returns an array including all substrings derived from s.
-    List<String> DFS(String s, List<String> wordDict, Map<String, LinkedList<String>>map) {
-        if (map.containsKey(s))
-            return map.get(s);
-
-        LinkedList<String>res = new LinkedList<String>();
-        if (s.length() == 0) {
-            res.add("");
-            return res;
-        }
-        for (String word : wordDict) {
-            if (s.startsWith(word)) {
-                List<String>sublist = DFS(s.substring(word.length()), wordDict, map);
-                for (String sub : sublist)
-                    res.add(word + (sub.isEmpty() ? "" : " ") + sub);
-            }
-        }
-        map.put(s, res);
-        return res;
-    }
-
-    /**
      * Bydirectional BFS
      * time: O(n)
      * space: O(n)
