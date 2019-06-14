e76d4bff2947070d324a32eb2a6d3fe2b3fd6bf0
LazyIce <1542752218@qq.com>
2019-06-13 23:48:58 -0700

    one problem

--- leetcode/_135_Candy.java
+++ leetcode/_135_Candy.java
@@ -1,7 +1,34 @@
-import java.util.Map;
-import java.util.HashMap;
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class _135_Candy {
+    /**
+     * Dynamic Programming
+     * time: O(n)
+     * space: O(n)
+     */
+    public int candy(int[] ratings) {
+        int[] candies = new int[ratings.length];
+        Arrays.fill(candies, 1); // give one candy to each person at first
 
+        // go from left to right
+        // add candy for person who has higher rating than left person
+        for (int i = 1; i < candies.length; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+        // go from right to left
+        // add candy for person who has higher rating than right person
+        for (int i = candies.length - 2; i >= 0; i --) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+
+        int res = 0;
+        for (int candy : candies) {
+            res += candy;
+        }
+        return res;
+    }
 }
