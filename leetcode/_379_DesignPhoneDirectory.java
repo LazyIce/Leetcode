dd2a7e1b35661e9b1aae85c1f511da984d0c1394
LazyIce <1542752218@qq.com>
2019-07-21 00:16:09 -0700

    nine problems

--- leetcode/_379_DesignPhoneDirectory.java
+++ leetcode/_379_DesignPhoneDirectory.java
@@ -1,2 +1,46 @@
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Set;
+import java.util.HashSet;
+
 public class _379_DesignPhoneDirectory {
+    class PhoneDirectory {
+        private Queue<Integer> queue;
+        private Set<Integer> used;
+        private int maxNumber;
+        /** Initialize your data structure here
+         @param maxNumbers - The maximum numbers that can be stored in the phone directory. */
+        public PhoneDirectory(int maxNumbers) {
+            queue = new LinkedList<>();
+            used = new HashSet<>();
+            maxNumber = maxNumbers;
+            for (int i = 0; i < maxNumbers; i++) {
+                queue.offer(i);
+            }
+        }
+
+        /** Provide a number which is not assigned to anyone.
+         @return - Return an available number. Return -1 if none is available. */
+        public int get() {
+            Integer number = queue.poll();
+            if (number == null)
+                return -1;
+            used.add(number);
+            return number;
+        }
+
+        /** Check if a number is available or not. */
+        public boolean check(int number) {
+            if (number >= maxNumber || number < 0)
+                return false;
+            return !used.contains(number);
+        }
+
+        /** Recycle or release a number. */
+        public void release(int number) {
+            if (used.remove(number)) {
+                queue.offer(number);
+            }
+        }
+    }
 }
--- leetcode/_380_InsertDeleteGetRandom.java
+++ leetcode/_380_InsertDeleteGetRandom.java
@@ -1,2 +1,46 @@
+import java.util.Random;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+
 public class _380_InsertDeleteGetRandom {
+    class RandomizedSet {
+        private Map<Integer, Integer> map; // key is the number, value is the index of number in list
+        private List<Integer> list;
+        private Random rand;
+        /** Initialize your data structure here. */
+        public RandomizedSet() {
+            map = new HashMap<>();
+            list = new ArrayList<>();
+            rand = new Random();
+        }
+
+        /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+        public boolean insert(int val) {
+            if (map.containsKey(val))
+                return false;
+            map.put(val, list.size());
+            list.add(val);
+            return true;
+        }
+
+        /** Removes a value from the set. Returns true if the set contained the specified element. */
+        public boolean remove(int val) {
+            if (!map.containsKey(val))
+                return false;
+            int index = map.remove(val);
+            int lastVal = list.remove(list.size() - 1);
+            if (index != list.size()) {
+                list.set(index, lastVal);
+                map.put(lastVal, index);
+            }
+            return true;
+        }
+
+        /** Get a random element from the set. */
+        public int getRandom() {
+            return list.get(rand.nextInt(list.size()));
+        }
+    }
 }
