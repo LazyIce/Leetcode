1dd39fb68f58f6a14bcfdc8114e1281fe15bfc70
LazyIce <1542752218@qq.com>
2019-07-23 00:02:39 -0700

    five problems

--- leetcode/_395_LongestSubstringwithAtLeastKRepeatingCharacters.java
+++ leetcode/_395_LongestSubstringwithAtLeastKRepeatingCharacters.java
@@ -1,26 +1,30 @@
 public class _395_LongestSubstringwithAtLeastKRepeatingCharacters {
     /**
+     * Sliding Window, Two pointers
      * time：O(n)
      * space: O(1)
      */
     public int longestSubstring(String s, int k) {
         if (s == null || s.length() == 0)
             return 0;
+
         char[] chs = new char[26];
         // record the frequency of each character
         for (int i = 0; i < s.length(); i++)
             chs[s.charAt(i) - 'a']++;
+
+        // return the length of string if this string is a valid string
         boolean flag = true;
         for (int i = 0; i < chs.length; i++) {
             if (chs[i] < k && chs[i] > 0)
                 flag = false;
         }
-        // return the length of string if this string is a valid string
         if (flag == true)
             return s.length();
+
+        // otherwise we use all the infrequent elements as splits
         int res = 0;
         int start = 0, cur = 0;
-        // otherwise we use all the infrequent elements as splits
         while (cur < s.length()) {
             if (chs[s.charAt(cur) - 'a'] < k) {
                 res = Math.max(res, longestSubstring(s.substring(start, cur), k));
@@ -29,6 +33,7 @@ public class _395_LongestSubstringwithAtLeastKRepeatingCharacters {
             cur++;
         }
         res = Math.max(res, longestSubstring(s.substring(start), k));
+
         return res;
     }
 }
