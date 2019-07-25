731e0e5c0e37e9b122793b253abda668d135c11f
LazyIce <1542752218@qq.com>
2019-07-25 00:11:34 -0700

    three problems

--- leetcode/_621_TaskScheduler.java
+++ leetcode/_621_TaskScheduler.java
@@ -1,9 +1,56 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
 public class _621_TaskScheduler {
+    /**
+     * time: O(n)
+     * space: O(n)
+     */
+    public int leastInterval1(char[] tasks, int n) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < tasks.length; i++) {
+            // map key is TaskName, and value is number of times to be executed.
+            map.put(tasks[i], map.getOrDefault(tasks[i], 0) + 1);
+        }
+
+        PriorityQueue<Map.Entry<Character, Integer>> pq = new PriorityQueue<>( //frequency sort
+                (a,b) -> a.getValue() != b.getValue() ? b.getValue() - a.getValue() : a.getKey() - b.getKey());
+
+        pq.addAll(map.entrySet());
+
+        int count = 0;
+        while (!pq.isEmpty()) {
+            int k = n + 1;
+            List<Map.Entry> tempList = new ArrayList<>();
+            while (k > 0 && !pq.isEmpty()) {
+                Map.Entry<Character, Integer> top = pq.poll(); // most frequency task
+                top.setValue(top.getValue() - 1); // decrease frequency, meaning it got executed
+                tempList.add(top); // collect task to add back to queue
+                k--;
+                count++; //successfully executed task
+            }
+
+            for (Map.Entry<Character, Integer> e : tempList) {
+                if (e.getValue() > 0)
+                    pq.add(e); // add valid tasks
+            }
+
+            if (pq.isEmpty())
+                break;
+            count = count + k; // if k > 0, then it means we need to be idle
+        }
+
+        return count;
+    }
+
     /**
      * time: O(n)
      * space: O(1)
      */
-    public int leastInterval(char[] tasks, int n) {
+    public int leastInterval2(char[] tasks, int n) {
         int[] counter = new int[26];
         int max = 0;
         int maxCount = 0;
