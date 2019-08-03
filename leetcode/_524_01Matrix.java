2234ee2d454cf85597bc5ffd4a2a6179cf9a34cb
LazyIce <1542752218@qq.com>
2019-08-02 23:56:48 -0700

    two problems

---
+++ leetcode/_524_01Matrix.java
@@ -0,0 +1,42 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class _524_01Matrix {
+    /**
+     * BFS
+     * time: O(m*n)
+     * space: O(m*n)
+     */
+    public int[][] updateMatrix(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    queue.offer(new int[]{i, j});
+                } else {
+                    matrix[i][j] = Integer.MAX_VALUE;
+                }
+            }
+        }
+
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        while (!queue.isEmpty()) {
+            int[] point = queue.poll();
+            for (int[] dir : dirs) {
+                int r = point[0] + dir[0];
+                int c = point[1] + dir[1];
+                if (r < 0 || r >= m || c < 0 || c >= n ||
+                        matrix[r][c] <= matrix[point[0]][point[1]] + 1) {
+                    continue;
+                }
+                queue.offer(new int[]{r, c});
+                matrix[r][c] = matrix[point[0]][point[1]] + 1;
+            }
+        }
+
+        return matrix;
+    }
+}
