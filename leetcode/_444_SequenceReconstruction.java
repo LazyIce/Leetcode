e41fea09d569300c32456a3dc5353f02b625938d
LazyIce <1542752218@qq.com>
2019-08-22 23:37:52 -0400

    three problems

---
+++ leetcode/_444_SequenceReconstruction.java
@@ -0,0 +1,67 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class _444_SequenceReconstruction {
+    /**
+     * Topology Sort
+     * time: O(V+E)
+     * space: O(V+E)
+     */
+    public boolean sequenceReconstruction(int[] org, List<List<Integer>> seqs) {
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        Map<Integer, Integer> indegree = new HashMap<>();
+
+        for(List<Integer> seq: seqs) {
+            if(seq.size() == 1) {
+                if(!map.containsKey(seq.get(0))) {
+                    map.put(seq.get(0), new HashSet<>());
+                    indegree.put(seq.get(0), 0);
+                }
+            } else {
+                for(int i = 0; i < seq.size() - 1; i++) {
+                    if(!map.containsKey(seq.get(i))) {
+                        map.put(seq.get(i), new HashSet<>());
+                        indegree.put(seq.get(i), 0);
+                    }
+
+                    if(!map.containsKey(seq.get(i + 1))) {
+                        map.put(seq.get(i + 1), new HashSet<>());
+                        indegree.put(seq.get(i + 1), 0);
+                    }
+
+                    if(map.get(seq.get(i)).add(seq.get(i + 1))) {
+                        indegree.put(seq.get(i + 1), indegree.get(seq.get(i + 1)) + 1);
+                    }
+                }
+            }
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (Map.Entry<Integer, Integer> entry: indegree.entrySet()) {
+            if (entry.getValue() == 0)
+                queue.offer(entry.getKey());
+        }
+
+        int index = 0;
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            if(size > 1)
+                return false;
+            int curr = queue.poll();
+            if(index == org.length || curr != org[index++])
+                return false;
+            for(int next: map.get(curr)) {
+                indegree.put(next, indegree.get(next) - 1);
+                if(indegree.get(next) == 0)
+                    queue.offer(next);
+            }
+        }
+
+        return index == org.length && index == map.size();
+    }
+}
