79d6cec3c76803ae8e41d56504d2d8a30d48b520
LazyIce <1542752218@qq.com>
2019-09-08 00:41:14 -0400

    two  problems

--- practices/social/TwitterSocialNetwork.java
+++ practices/social/TwitterSocialNetwork.java
@@ -5,6 +5,30 @@ public class TwitterSocialNetwork {
     }
 
     public static int countGroups(String[] related) {
-        return 0;
+        int[][] M = new int[related.length][related.length];
+        for (int i = 0; i < related.length; i++) {
+            for (int j = 0; j < related[i].length(); j++) {
+                M[i][j] = related[i].charAt(j) - '0';
+            }
+        }
+
+        int[] visited = new int[M.length];
+        int count = 0;
+        for (int i = 0; i < M.length; i++) {
+            if (visited[i] == 0) {
+                dfs(M, visited, i);
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void dfs(int[][] M, int[] visited, int i) {
+        for (int j = 0; j < M.length; j++) {
+            if (M[i][j] == 1 && visited[j] == 0) {
+                visited[j] = 1;
+                dfs(M, visited, j);
+            }
+        }
     }
 }
