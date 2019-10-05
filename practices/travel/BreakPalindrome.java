d5ff208459b98def0138b78a3a8d59c7a2cccd6f
LazyIce <1542752218@qq.com>
2019-10-04 23:53:46 -0400

    eight problems

--- practices/travel/BreakPalindrome.java
+++ practices/travel/BreakPalindrome.java
@@ -1,5 +1,17 @@
 public class BreakPalindrome {
     public static void main(String[] args) {
-        String s = "bcb";
+        String s = "aabaa";
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != 'a') {
+                if (s.length() % 2 != 0 && i == s.length() / 2) {
+                    System.out.println("IMPOSSIBLE");
+                    break;
+                } else {
+                    System.out.println(s.substring(0, i) + 'a' + s.substring(i + 1));
+                    break;
+                }
+            }
+        }
     }
 }
