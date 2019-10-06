c7261fbb4699b29b4c339ce7167cec054cf2acef
LazyIce <1542752218@qq.com>
2019-10-06 01:26:48 -0400

    three problems

--- practices/search/MergeSort.java
+++ practices/search/MergeSort.java
@@ -1,6 +1,56 @@
+import java.util.Arrays;
+
 public class MergeSort {
     public static void main(String[] args) {
         int[] arr = new int[] {6, 3, 1, 7, 12, 8};
+        sort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
 
+    public static void sort(int[] arr, int l, int r) {
+        if (l < r) {
+            int m = (r - l) / 2 + l;
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+            merge(arr, l, m, r);
+        }
+    }
+
+    public static void merge(int[] arr, int l, int m, int r) {
+        int len1 = m - l + 1;
+        int len2 = r - m;
+        int[] tempL = new int[len1];
+        int[] tempR = new int[len2];
+        for (int i = 0; i < len1; i++) {
+            tempL[i] = arr[l + i];
+        }
+        for (int j = 0; j < len2; j++) {
+            tempR[j] = arr[j + m + 1];
+        }
+
+        int i = 0, j = 0;
+        int k = l;
+        while (i < len1 && j < len2) {
+            if (tempL[i] <= tempR[j]) {
+                arr[k] = tempL[i];
+                i++;
+            } else {
+                arr[k] = tempR[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < len1) {
+            arr[k] = tempL[i];
+            i++;
+            k++;
+        }
+
+        while (j < len2) {
+            arr[k] = tempR[j];
+            j++;
+            k++;
+        }
     }
 }
--- practices/search/QuickSort.java
+++ practices/search/QuickSort.java
@@ -1,5 +1,41 @@
+import java.util.Arrays;
+
 public class QuickSort {
     public static void main(String[] args) {
-
+        int[] arr = new int[] {6, 3, 1, 7, 12, 8};
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
     }
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+
+            quickSort(arr, low, pi - 1);  // Before pi
+            quickSort(arr, pi + 1, high); // After pi
+        }
+    }
+
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1; // index of smaller element
+        for (int j = low; j < high; j++) {
+            // If current element is smaller than or
+            // equal to pivot
+            if (arr[j] <= pivot) {
+                i++;
+                // swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        // swap arr[i+1] and arr[high] (or pivot)
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
 }
