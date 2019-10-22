6ef07adab7e902c8ff95ee9b46c1fd8039c06b66
LazyIce <1542752218@qq.com>
2019-10-21 21:57:17 -0700

    four problems

--- leetcode/_698_PartitiontoKEqualSumSubsets.java
+++ leetcode/_698_PartitiontoKEqualSumSubsets.java
@@ -1,4 +1,8 @@
 public class _698_PartitiontoKEqualSumSubsets {
+    /**
+     * time: O(2^n)
+     * space: O(n)
+     */
     public boolean canPartitionKSubsets(int[] nums, int k) {
         int sum = 0;
         for (int num : nums) {
@@ -8,25 +12,26 @@ public class _698_PartitiontoKEqualSumSubsets {
             return false;
         }
         int[] visited = new int[nums.length];
-        return canPartition(nums, visited, 0, k, 0, 0, sum/k);
+        return canPartition(nums, visited, 0, k, 0, sum/k);
     }
 
-    public boolean canPartition(int[] nums, int[] visited, int start_index, int k, int cur_sum, int cur_num, int target){
+    public boolean canPartition(int[] nums, int[] visited, int start, int k, int cur_sum, int target) {
         if (k == 1) {
             return true;
         }
-        if (cur_sum == target && cur_num > 0) {
-            return canPartition(nums, visited, 0, k-1, 0, 0, target);
+        if (cur_sum == target) {
+            return canPartition(nums, visited, 0, k - 1, 0, target);
         }
-        for (int i = start_index; i < nums.length; i++) {
-            if(visited[i] == 0) {
+        for (int i = start; i < nums.length; i++) {
+            if (visited[i] == 0) {
                 visited[i] = 1;
-                if (canPartition(nums, visited, i+1, k, cur_sum + nums[i], cur_num++, target)) {
+                if (canPartition(nums, visited, i + 1, k, cur_sum + nums[i], target)) {
                     return true;
                 }
                 visited[i] = 0;
             }
         }
+
         return false;
     }
 }
