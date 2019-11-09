80e03bac13873872cde79e927ab9cd07190628f1
LazyIce <1542752218@qq.com>
2019-11-08 22:24:49 -0800

    four problems

---
+++ leetcode/_416_PartitionEqqualSubsetSum.java
@@ -0,0 +1,71 @@
+import java.util.Arrays;
+
+public class _416_PartitionEqqualSubsetSum {
+    /**
+     * time: O(n)
+     * space: O(n*sum)
+     */
+    public boolean canPartition1(int[] nums) {
+        int sum = 0;
+
+        for (int num : nums) {
+            sum += num;
+        }
+
+        if ((sum & 1) == 1) {
+            return false;
+        }
+        sum /= 2;
+
+        int n = nums.length;
+        boolean[][] dp = new boolean[n+1][sum+1];
+        for (int i = 0; i < dp.length; i++) {
+            Arrays.fill(dp[i], false);
+        }
+
+        dp[0][0] = true;
+
+        for (int i = 1; i < n+1; i++) {
+            dp[i][0] = true;
+        }
+        for (int j = 1; j < sum+1; j++) {
+            dp[0][j] = false;
+        }
+
+        for (int i = 1; i < n+1; i++) {
+            for (int j = 1; j < sum+1; j++) {
+                dp[i][j] = dp[i-1][j];
+                if (j >= nums[i-1]) {
+                    dp[i][j] = (dp[i][j] || dp[i-1][j-nums[i-1]]);
+                }
+            }
+        }
+
+        return dp[n][sum];
+    }
+    /**
+     * time: O(n)
+     * space: O(n)
+     */
+    public boolean canPartition2(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        if (sum % 2 != 0) {
+            return false;
+        }
+        sum = sum / 2;
+        boolean[] dp = new boolean[sum+1];
+        Arrays.fill(dp, false);
+        dp[0] = true;
+        for (int num : nums) {
+            for (int i = sum; i > 0; i--) {
+                if (i >= num) {
+                    dp[i] = dp[i] || dp[i-num];
+                }
+            }
+        }
+        return dp[sum];
+    }
+}
